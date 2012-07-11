384ed883012338146b52f726c9b2632ffb09d0a8
Fengfei Wang <fengfei1000@gmail.com>
2012-07-11 09:31:01 +0800

    refactor

---
+++ forest-database/src/example/java/fengfei/forest/slice/database/Ex.java
@@ -0,0 +1,14 @@
+package fengfei.forest.slice.database;
+
+
+public class Ex {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
